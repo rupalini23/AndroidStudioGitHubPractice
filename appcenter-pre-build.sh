81e228741b8019dbfa04c2605f85d02b525ab292
Rupa <rupalini23.senapati@gmail.com>
2020-08-05 22:32:40 +0530

    Delete appcenter-pre-build.sh

--- appcenter-pre-build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Example: Change bundle name of an iOS app for non-production
-if [ "$APPCENTER_BRANCH" != "master" ];
-then
-echo "pre script is started execution"
-    plutil -replace CFBundleName -string "\$(PRODUCT_NAME) Beta" $APPCENTER_SOURCE_DIRECTORY/MyApp/Info.plist
-fi
