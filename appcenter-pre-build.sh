8815161c6d67a491e2567297fbb7cfc047638afa
Rupa <rupalini23.senapati@gmail.com>
2020-08-05 19:43:07 +0530

    Update appcenter-pre-build.sh

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -3,6 +3,7 @@
 # Example: Change bundle name of an iOS app for non-production
 if [ "$APPCENTER_BRANCH" != "master" ];
 then
+    echo "pre is started executing"
     plutil -replace CFBundleName -string "\$(PRODUCT_NAME) Beta" $APPCENTER_SOURCE_DIRECTORY/MyApp/Info.plist
 fi
 
